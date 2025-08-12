e283b0f05b6fac6bf380f7aa58b53ccc63df50a6
Kira Oliveira <kira.oliveira.web@gmail.com>
2025-08-12 06:57:03 -0300

    Criando step para abrir o navegador e o site

--- src/test/java/app/vercel/walkdog/register/newuser/NewUserTests.java
+++ src/test/java/app/vercel/walkdog/register/newuser/NewUserTests.java
@@ -1,4 +1,26 @@
 package app.vercel.walkdog.register.newuser;
 
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+@DisplayName("Testes Automatizados da Funcionalidade de Novo Walker")
 public class NewUserTests {
+    @Test
+    @DisplayName("Criando um novo Walker com dados válidos")
+    public void testCriandoUmNovoWalkerComDadosValidos() {
+        // Aqui o navegador Chrome será aberto
+        WebDriverManager.chromedriver().setup();
+        WebDriver navegador = new ChromeDriver();
+        navegador.manage().window().maximize();
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+
+        // Aqui o site Walkdog será aberto
+        navegador.get("https://walkdog.vercel.app/");
+    }
 }
